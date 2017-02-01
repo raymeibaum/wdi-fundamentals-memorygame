95685ba32decc28eebb39c8ff721052ac315817d
Ray Meibaum <rmeibaum@me.com>
2017-02-01 14:28:07 -0500

    if else logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = 'queen',
     cardTwo = 'queen',
     cardThree = 'king',
     cardFour = 'king';
+
+if (cardOne === cardTwo) {
+  alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
