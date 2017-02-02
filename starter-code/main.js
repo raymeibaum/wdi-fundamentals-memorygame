62cab9b659521c0f1b7e9fa3c61d32f5f78f09fe
Ray Meibaum <rmeibaum@me.com>
2017-02-02 15:08:13 -0500

    create cards in javaScript using function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,20 @@ var cardOne = 'queen',
     cardThree = 'king',
     cardFour = 'king';
 
-if (cardOne === cardTwo) {
-  alert('You found a match!');
-} else {
-  alert('Sorry, try again.');
+var gameBoard = document.getElementById('game-board');
+
+// if (cardOne === cardTwo) {
+//   alert('You found a match!');
+// } else {
+//   alert('Sorry, try again.');
+// }
+
+function createCards() {
+  for (var i = 0; i < 4; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
 }
+
+createCards();
