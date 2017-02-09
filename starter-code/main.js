51afe6d55bf85f7e19b4d18a38c306bdacb7728a
Ray Meibaum <rmeibaum@me.com>
2017-02-08 20:41:28 -0500

    BROKEN CODE (switching computers

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,37 @@
-var cardOne = 'queen',
-    cardTwo = 'queen',
-    cardThree = 'king',
-    cardFour = 'king';
+var cards = ['queen', 'queen', 'king', 'king'],
+    cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
 
-// if (cardOne === cardTwo) {
-//   alert('You found a match!');
-// } else {
-//   alert('Sorry, try again.');
-// }
-
-function createCards() {
-  for (var i = 0; i < 4; i++) {
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
     var newCard = document.createElement('div');
     newCard.className = 'card';
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', isTwoCards);
     gameBoard.appendChild(newCard);
   }
 }
 
-createCards();
+function isMatch(arr) {
+  return arr[0] === arr[1];
+}
+
+function isTwoCards() {
+  if (this.getAttribute('data-card') === 'king') {
+    this.innerHTML = '<img src="king.png" alt="King">';
+  } else {
+    this.innerHTML = '<img src="queen.png" alt="Queen">';
+  }
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    var cards = document.getElementsByClassName('card');
+    console.log(cards);
+    cards.innerHTML = "";
+    console.log(cards);
+    cardsInPlay = [];
+  }
+}
+
+createBoard();
